6d7ae4f8f65e85f1aaf1443f8b39afcf6311669d
rahul saini <rrahulsaini06@gmail.com>
2024-04-10 00:08:28 +0530

    My first api end point with express

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-console.log("hello i'm starting a new NodeJS project")
-const { createServer } = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// console.log("hello i'm starting a new NodeJS project")
+// const { createServer } = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello rahul saini')
+})
+
+app.get('/mobile', function (req, res) {
+    res.send('this is my mobile number: 7691080706')
+  })
+
+app.listen(3000)
